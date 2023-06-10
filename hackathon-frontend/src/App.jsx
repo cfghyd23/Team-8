466be0085f17f5226c3e29c44e35c5f814c4f91b
Crack-er-jack <osmanimadhavi@gmail.com>
2023-06-11 00:10:51 +0530

    Routing and Sidenav

--- hackathon-frontend/src/App.jsx
+++ hackathon-frontend/src/App.jsx
@@ -1,19 +1,35 @@
 import FirstPage from "./components/FirstPage/FirstPage";
 import Sidebar from './components/Dashboard/Sidebar';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./components/SecondPage/Login";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+const Layout = ({children}) => {
+  console.log(window.location.href)
+
+      if (window.location.href != "http://127.0.0.1:5173/dashboard"){
+        return (<div>{children}</div>);
+}
+else {
+  return (
+    <div>
+<Sidebar/>
+{children}</div>);
+    
+}
+}
+
 function App() {
   return (
-    
     <BrowserRouter>
-    
-    <Routes>
-      <Route path="/">
-        <Route index element={<FirstPage />} />
-        <Route path="/dashboard" element={<Sidebar />}></Route>
-      </Route>
-
+    <Layout>
+      <Routes>
+        {/* Replace FirstPage with dash components */}
+      <Route path="/dashboard" Component={FirstPage} ></Route> 
+    <Route path="/LandingPage" Component={FirstPage}></Route>
+    <Route path="/Login" Component={LoginPage}></Route>
     </Routes>
-  </BrowserRouter>
+  </Layout>
+  </BrowserRouter>    
     // <Sidebar />
   );
 }
