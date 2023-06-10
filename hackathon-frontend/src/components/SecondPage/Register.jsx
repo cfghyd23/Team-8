236c1c793b509948f5bb505e2704b1d700a61ef6
kavya <kavyasreelakshmi78@gmail.com>
2023-06-11 00:37:54 +0530

    added all the required elements to the Register form

--- hackathon-frontend/src/components/SecondPage/Register.jsx
+++ hackathon-frontend/src/components/SecondPage/Register.jsx
@@ -17,11 +17,19 @@ export default function Example() {
         <Typography color="gray" className="mt-1 font-normal">
           Enter your details to register.
         </Typography>
-        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96 h-screen">
           <div className="mb-4 flex flex-col gap-6">
-            <Input size="lg" label="Name" />
-            <Input size="lg" label="Email" />
+            <Input type="text" size="lg" label="Name" />
+            <Input type="text" size="lg" label="Email" />
+            <Input type="tel" size="lg" label="Phone Number" />
             <Input type="password" size="lg" label="Password" />
+            <Input type="date" size="lg" label="Date of Birth" />
+            <Input type="text" size="lg" label="Gender" />
+            <Input type="text" size="lg" label="City" />
+            <Input type="text" size="lg" label="State" />
+            <Input type="text" size="lg" label="Segment" />
+            <Input type="text" size="lg" label="Cause" />
+            
           </div>
           
           <Button className="mt-6" fullWidth>
